7c83f3d46625385f38dcc04929a7d4a24ce16354
yuanyue <87439247@qq.com>
2018-07-30 11:11:18 +0800

    按照老版修改

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
-
-server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
+var server = prerender({
+    logRequests: true
+});
+// server.use(prerender.basicAuth());
+// server.use(prerender.sendPrerenderHeader());
+server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(prerender.redisCache());
 
 server.start();
